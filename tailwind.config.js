ac8258645151b2528560041aaba683cd6011524a
hellokidas <ghazaislami8@gmail.com>
2024-11-15 22:48:25 +0700

    Ipdate v1.1

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['index.html'],
+  content: ["index.html"],
   theme: {
     extend: {
       colors: {
-        primary '#14b8a6'
-      }
+        primary: "#14b8a6",
+        dark: "#0f172a",
+      },
     },
   },
   plugins: [],
-}
-
+};
